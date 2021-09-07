d19ff8f3cf4764eba095b5922d7e596de6440b63
Kenny <kenny@anhtrinh.com>
2021-09-07 16:05:55 +0700

    1st commit

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot
+
+def hello(name):
+    print(f'Hello {name}')
+
+def plot_loss_curve(model_history):
+    """
+    Plot loss curve of function & metrics on a seperate plot
+    :param model_history: a
+    :return: Plots of accuracy & metrics
+    """
+
