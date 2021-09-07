0c82a4b29f68b002314381b969ab823be0dc4ae9
Kenny <kenny@anhtrinh.com>
2021-09-07 16:26:38 +0700

    update plot loss

--- helpers.py
+++ helpers.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot
-
-def hello(name):
-    print(f'Hello {name}')
+import matplotlib.pyplot as plt
 
 def plot_loss_curve(model_history):
     """
@@ -10,4 +7,29 @@ def plot_loss_curve(model_history):
     :param model_history: a
     :return: Plots of accuracy & metrics
     """
+    df = pd.DataFrame(model_history.history)
+    epochs = range(1,len(df)+1)
+
+    plt.figure(figsize=(10,5))
+
+    # Setup loss curve
+    plt.subplot(121)
+    plt.plot(epochs, df['loss'],label = 'Train loss')
+    plt.plot(epochs, df['val_loss'], label = 'Validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Loss')
+    plt.legend()
+
+    #Setup accuracy_curve
+    plt.subplot(122)
+    plt.plot(epochs, df['accuracy'],label = 'Train accuracy')
+    plt.plot(epochs, df['val_accuracy'],label = 'Test accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Accuracy')
+
+    plt.legend()
+    plt.show()
+
 
